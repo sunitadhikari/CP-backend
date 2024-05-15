51212b1b6750ef234b2ac65b2957b3e42fd13716
sunitadhikari <sunitaadhikari2001@gmail.com>
2024-05-15 22:51:15 +0545

    postlogin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,26 +3,52 @@ const router = express.Router()
 const jwt = require('jsonwebtoken')
 const user = require('../models/userModel');
 
-router.post('/userSignup', async(req, res) => {
-    try{
+router.post('/userSignup', async (req, res) => {
+    try {
 
-    
-    const newUser = new user({
-        username:req.body.username,
-        email:req.body.email,
-        password:req.body.password,
-        confirmPassword:req.body.confirmPassword
-    })
-    const passwordCheck = newUser.password == newUser.confirmPassword
-    if(!passwordCheck){
-        return res.json({message:'password doesnot match'})
+
+        const newUser = new user({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            confirmPassword: req.body.confirmPassword
+        })
+        const passwordCheck = newUser.password == newUser.confirmPassword
+        if (!passwordCheck) {
+            return res.json({ message: 'password doesnot match' })
+        }
+        await newUser.savse();
+        res.json({ message: 'Password saved' })
     }
-    await newUser.save();
-    res.json({message:'Password saved'})
-}
-catch(error){
-    console.log(error);
-}
+    catch (error) {
+        console.log(error);
+    }
 })
 
+
+router.get('/getUserSignup', (req, res) => {
+    user.find()
+        .then(data => res.send(data))
+        .catch(err => console.log(err))
+})
+
+
+router.post('/postLogin', async (req, res) => {
+    const { username, password } = req.body;
+    user.findOne({ username });
+    if (!userData) {
+        return res.status(401)({ message: 'username not found' })
+    }
+    const isPasswordMatch = password === userData.confirmPassword;
+    try{
+        if (!isPasswordMatch) {
+            console.log('password is incorrect');
+            return res.status(401).json({ message: 'Incorrect password' });
+        }  
+    }
+    catch(error){
+        console.log(error);
+        res.json({message:'Internal server error'})
+    }
+})
 module.exports = router;
