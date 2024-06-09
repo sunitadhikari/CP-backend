64411f310c668a22439a16e7a0e57e3e99b73c9e
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-06-09 13:14:20 +0545

    middleware

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mongodb = require('mongodb')
 
 // mongoose.connect("mongodb+srv://anish:anish@cluster0.xuf0z19.mongodb.net/restaurantsun", {
 mongoose.connect("mongodb+srv://sunitaadhikari2001:sunita@cluster0.kjvmfmf.mongodb.net/hospital-automation", {
-    useNewUrlParser: true, //handle the parsing of the MongoDB connection string used in Mongodb Node.js Driver
-    useUnifiedTopology: true, //topology engine used in Mongodb Node.js Driver
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
     family: 4,
     })
 
