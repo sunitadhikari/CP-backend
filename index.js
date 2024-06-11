1e89122d022fac9b0f5609c407da5e1180400c0d
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-06-11 23:05:01 +0545

    department doctor implementaion

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const connectDb  = require('./db')
+const connectDb = require('./db')
 const cors = require('cors')
 const userRouter = require('./routes/userRoutes')
+const doctorRouter = require('./routes/doctorRoute')
+const departmentRoute = require('./routes/departmentRoute')
 
 app.use(express.json())
 app.use(cors());
 app.use(userRouter);
-app.listen(3000, ()=>{
+app.use(doctorRouter);
+app.use(departmentRoute);
+app.listen(3000, () => {
     console.log('Localhost is connected to 3000')
 })
---
+++ models/doctorModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const doctorSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  department: { type: String },
+  picture: { type: String },
+  dob: { type: Date },
+  sex: { type: String, required: true },
+  bloodGroup: { type: String },
+  designation: { type: String, required: true },
+  address: { type: String, required: true },
+  phoneNo: { type: String },
+  mobileNo: { type: String, required: true },
+  careerTitle: { type: String, required: true },
+  biography: { type: String },
+  status: { type: String, required: true }
+});
+
+const Doctor = mongoose.model('Doctor', doctorSchema);
+
+module.exports = Doctor;
