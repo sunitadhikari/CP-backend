0d0fd578967e0e25bf56772e6562c835b7fae267
sunitadhikari <sunitaadhikari2001@gmail.com>
2024-06-18 23:01:13 +0545

    api

--- models/scheduleModel.js
+++ models/scheduleModel.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 const scheduleSchema = new mongoose.Schema({
-    availableDaya:{type:String, require:true},
+    doctorName:{type:String, require:true},
+    availableDays:{type:String, require:true},
     startTime:{type:String, require:true},
     endTime:{type:String, require:true},
     mobileNumber:{type:Number, require:true},
-    password:{type:String, require:true},
-    bloodGroup:{type:String},
-    sex:{type: String, require: true}, 
-    dob:{type:String, require:true}
+    sex:{type: String, require: true} 
 })
 const schedule = mongoose.model('schedule', scheduleSchema);
 module.exports = schedule;
--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const scheddule = require('../models/labModel')
+const schedule = require('../models/scheduleModel')
 
+router.post('/postSchedule', async(req, res)=>{
+    try{
+       const schedules = new schedule(req.body);
+       await schedules.save();
+       res.status(201).json(schedules);
+    }
+    catch(err){
+     res.status(400).json({message:err.message})
+    }
+})
+router.get('/getSchedule', async(req, res)=>{
+    try{
+        const schedul = await schedule.find();
+        res.json(schedul);
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+})
 
 module.exports = router;
