7b383f0ccc48ca13c056ee34ffd47210d0c0c60e
Menuka Lamsal <lamshalashmita2020@gmail.com>
2024-07-10 23:42:43 -0500

    added registereddate and isVerified field on userModel

--- models/userModel.js
+++ models/userModel.js
@@ -20,6 +20,8 @@ const userSchema = new mongoose.Schema({
     password: { type: String },
     confirmPassword: { type: String },
     termCondition: { type: Boolean },
+    registereddate:{ type: String },
+    isVerified:{ type:Boolean, required:true },
     role: {
         type: String,
         enum: ["doctor", "patient", "admin", "labtec"],
