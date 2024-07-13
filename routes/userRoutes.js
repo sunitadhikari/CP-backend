2b05f68c6ea999441b18e058161c406cd510e0c9
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-07-14 00:16:38 +0545

    proilr

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -152,5 +152,16 @@ router.get('/getusersdatabyEmail', verifyToken, async (req, res) =>{
       res.status(500).send(error);
     }
   });
+  router.get('/profile', verifyToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', error });
+    }
+});
 
 module.exports = router;
