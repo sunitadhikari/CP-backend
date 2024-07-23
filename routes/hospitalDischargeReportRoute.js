04ffdcff15270b763ce8703f128dbb91b04b686b
Menuka Lamsal <lamshalashmita2020@gmail.com>
2024-07-23 07:54:56 -0500

    gethospitaldischargereport

--- routes/hospitalDischargeReportRoute.js
+++ routes/hospitalDischargeReportRoute.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const HospitalDischargeReport = require('../models/hospitalDischargeReportModel');
 const verifyToken=require('../middleware');
+const Signup = require('../models/userModel');
+const AdmissionPatient= require('../models/admissionPatientModel');
+const Bed = require('../models/bedModel');
 
 // POST a new hospital discharge report
 router.post('/hospitalDischargeReport', verifyToken, async (req, res) => {
@@ -14,14 +17,96 @@ router.post('/hospitalDischargeReport', verifyToken, async (req, res) => {
     }
 });
 
-router.get('/gethospitalDischargeReport', verifyToken, async(req,res)=>{
-    try{
-       const report=await HospitalDischargeReport.find();
-       res.status(200).json(report);
-    }catch(error){
-        res.status(500).json({ message: "Internal server error!",error:error.message });
+router.get('/gethospitalDischargeReport', async (req, res) => {
+    try {
+        const reports = await HospitalDischargeReport.find();
+
+        const reportWithDetails = await Promise.all(reports.map(async report => {
+            const [firstName, lastName] = report.patientName.split(' ');
+            const user = await Signup.findOne({ firstName, lastName });
+            if (!user ) {
+                return { ...report._doc, error: "Patient not found" };
+            }
+
+            const admissionData = await AdmissionPatient.findOne({ firstName: user.firstName, lastName: user.lastName });
+            if (!admissionData) {
+                return { ...report._doc, error: "Admission Data not found for this patient" };
+            }
+
+            const bedData = await Bed.findOne({ bedNumbers: admissionData.bedNumber });
+            if (!bedData) {
+                return { ...report._doc, error: "No bed found for this user" };
+            }
+
+            return {
+                ...report._doc,
+                    //firstName: user.firstName,
+                    //lastName: user.lastName,
+                    email: user.email,
+                    department: admissionData.department,
+                    ward: bedData.ward,
+                    bedNumber: bedData.bedNumbers,
+                    charges: bedData.charges,
+                    admittedAt: admissionData.admittedAt,
+                    dischargeDate: admissionData.dischargeDate,
+                    checkedBy: admissionData.checkedBy
+                // patientDetails: {
+                //     firstName: user.firstName,
+                //     lastName: user.lastName,
+                //     email: user.email,
+                // },
+                // bedDetails: {
+                //     ward: bedData.ward,
+                //     bedNumber: bedData.bedNumbers,
+                //     charges: bedData.charges,
+                //     occupied: bedData.occupied
+                // },
+                // admissionDetails: {
+                //     department: admissionData.department,
+                //     ward: admissionData.ward,
+                //     bedNumber: admissionData.bedNumber,
+                //     admittedAt: admissionData.admittedAt,
+                //     dischargeDate: admissionData.dischargeDate,
+                //     checkedBy: admissionData.checkedBy
+                // }
+            };
+        }));
+
+        res.status(200).json(reportWithDetails);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error!", error: error.message });
     }
-})
+});
+
+// router.get('/gethospitalDischargeReport', async(req,res)=>{
+//     try{
+//        const report=await HospitalDischargeReport.find();
+//        const reportwithbed = await Promise.all(report.map(async reports =>{
+//             const user = await Signup.find({email:reports.patientName});
+//             if(!user || user.length === 0){
+//                 return res.status(404).send("Patient not found");
+//             }
+//             const bed=await AdmissionPatient.find(user.firstName && user.lastName);
+//             if(!bed || bed.length === 0){
+//                 return res.status(404).send("Admission Data not found for this patient");
+//             }
+//             const charge = await Bed.findOne({bedNumbers:bed.bedNumber});
+//             if(!charge || charge.length == 0)
+//             {
+//                 return res.status(404).send("No bed found for this user")
+//             }
+//             return{
+//                 ...user._doc,
+//                 bed:bed,
+//                 charge:charge,
+//             }
+//        }))
+       
+//        res.status(200).json(reportwithbed);
+//     }catch(error){
+//         res.status(500).json({ message: "Internal server error!",error:error.message });
+//     }
+// })
 
 router.get('/gethospitalDischargeReportbyEmail', verifyToken, async(req,res)=>{
     try{
